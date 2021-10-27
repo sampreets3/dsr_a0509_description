cabd6c10b6ad1fcc4c0c92c74e60d97c31813520
Sampreet <sampreets3@gmail.com>
2021-10-27 10:55:16 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from glob import glob
+
+package_name = 'dsr_a0509_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+            ('share/' + package_name, glob('launch/*.py')),
+    ('share/' + package_name+'/urdf/', glob('urdf/*')),
+    ('share/' + package_name+'/rviz/', glob('rviz/*')),
+    ('share/' + package_name+'/meshes/', glob('meshes/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Sampreet Sarkar',
+    maintainer_email='sampreets3@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
